8aed8f220b69fcdefe2ca4ab485aa54ef5b71d31
Martin Bless <martin.bless@mbless.de>
2019-05-06 14:30:38 +0200

    Bump version to 0.3.0-alpha

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/development.html#single-sourcing-the-version
-    version='0.2.0',
+    version='0.3.0-alpha',
 
     description='Implement t3-field-list-table Directive for Sphinx',
     long_description=long_description,
